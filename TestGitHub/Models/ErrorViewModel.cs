90228b750520f4e6d1f0ae26004ef1677f93963b
Jiayi <jiayi.gmail.com>
2023-04-28 14:20:30 +0800

    update

---
+++ TestGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TestGitHub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
